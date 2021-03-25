544f6d67c48d0965140906ad8c9c49049101062e
Basharalsayyed2000 <78122316+Basharalsayyed2000@users.noreply.github.com>
2021-03-26 01:46:38 +0200

    creating project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'Screens/Home.dart';
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+   @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Home.route,
+       routes: {
+       
+        Home.route: (context) => Home(),
+      },
+    );
+  }
+}
